4c2828d8dfd36470071a2cb1eccd19a57dba341d
anthonyw12123 <powderedeggs@gmail.com>
2022-05-15 16:15:33 -0700

    Added gitignore, pylint

--- FileSorter.py
+++ FileSorter.py
@@ -1,62 +1,76 @@
-def log(content):
-    print(content)
+"""
+:doc: `filesorter.py` is a file sorter, given a directory. Sorts by month.
+"""
 
-def GetTree(path):
-    output = dict()
-    files = glob.glob("*.*", root_dir=path)
-    for f in files:
-        lastModified = datetime.datetime.fromtimestamp(os.path.getmtime(f))
-        fullPath = os.path.join(path, f)
-        monthsFiles = output.setdefault(str(lastModified.month), [ ])
-        monthsFiles.append(fullPath)
-    return output
+import glob
+import datetime
+import os
+import argparse
 
-def GetDirectory(workingDirectory, month, execute):
-    directoryPath = os.path.join(workingDirectory, month)
-    if not os.path.exists(directoryPath) and execute:
-        os.mkdir(directoryPath)
-    print("Created " + directoryPath)
-    return directoryPath
-
-def MoveFile(workingDirectory,key, file, execute):
-    filename = os.path.basename(file)
-    newPath = os.path.join(workingDirectory, key, filename)
-    if execute:
-        os.rename(file, newPath)
-    print(file + " -> " + newPath)
-
-def SortTree(workingDirectory, tree, execute):
-    for month in tree:
-        target = GetDirectory(workingDirectory, month, execute)
-        for file in tree[month]:
-            MoveFile(workingDirectory, month, file, execute)
-    return
-
-def isDirectoryValidator(path):
+def is_directory_validator(path):
+    """validates a given directory"""
     if not os.path.isdir(path):
         raise argparse.ArgumentError
     return path
 
-if __name__ == '__main__':
-    import os
-    import argparse
-
-    currentDirectory = os.getcwd()
+def get_arguments():
+    """Gets the arguments for the utility."""
     parser = argparse.ArgumentParser(description ='Filesorter. Organizes directory by scheme.')
-    parser.add_argument('-r', help='Recursive. Defaults to false.', dest='recursive', default=False)
-    parser.add_argument('-x', help='Execute. Executes file actions. Defaults to false.', dest='execute', default=False)
-    parser.add_argument('-v', help='Verbose. Lists all actions taken. Defaults to false.', dest='verbose', default=False)
-    parser.add_argument('directory', type=isDirectoryValidator, nargs='?', default=currentDirectory, help='The directory in which to operate.')
+    parser.add_argument('-r',
+        help='Recursive. Defaults to false.',
+        dest='recursive',
+        default=False)
+    parser.add_argument('-x', 
+        help='Execute. Executes file actions. Defaults to false.',
+        dest='execute',
+        default=False)
+    parser.add_argument('-v',
+        help='Verbose. Lists all actions taken. Defaults to false.',
+        dest='verbose',
+        default=False)
+    parser.add_argument('directory',
+        type=is_directory_validator,
+        nargs='?',
+        default=os.getcwd(),
+        help='The directory in which to operate.')
+    parsed_args = parser.parse_args()
 
-    args = parser.parse_args()
+    return parsed_args
 
-    print(args.directory)
-    print(args.recursive)
+def get_tree(path):
+    """returns a dictionary tree of months and filenames, given a path"""
+    output = dict()
+    files = glob.glob("*.*", root_dir=path)
+    for file in files:
+        last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(f))
+        full_path = os.path.join(path, file)
+        months_files = output.setdefault(str(last_modified.month), [ ])
+        months_files.append(full_path)
+    return output
 
-    import glob
-    import datetime
+def get_directory_for_month(working_directory, month, execute):
+    """Creates target directory for month. Returns created directory path."""
+    directory_path = os.path.join(working_directory, month)
+    if not os.path.exists(directory_path) and execute:
+        os.mkdir(directory_path)
+    print("Created " + directory_path)
+    return directory_path
+def move_file(working_directory,key, file, execute):
+    """Moves a file to the target directory"""
+    filename = os.path.basename(file)
+    new_path = os.path.join(working_directory, key, filename)
+    if execute:
+        os.rename(file, new_path)
+    print(file + " -> " + new_path)
+def sort_tree(working_directory, directory_tree, execute):
+    """Enacts sorting on a given directory_tree."""
+    for month in directory_tree:
+        target = get_directory_for_month(working_directory, month, execute)
+        for file in directory_tree[month]:
+            move_file(working_directory, month, file, execute)
+    return
 
-    tree = GetTree(args.directory)
-
-    SortTree(args.directory , tree, args.execute)
-    
+if __name__ == '__main__':
+    args = get_arguments()
+    tree = get_tree(args.directory)
+    sort_tree(args.directory , tree, args.execute)
